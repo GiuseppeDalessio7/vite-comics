d2678b2f81b854b80a1b6fc541092baf874a95a6
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-09-22 17:20:09 +0200

    Finish header

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,22 @@ export default {
 </script>
 
 <template>
-  <img src="./assets/img/dc-logo.png" alt="">
-  <AppHeader></AppHeader>
+  <div class=" container p-3">
+    <div class="d-flex justify-content-between">
+      <img class="" src="./assets/img/dc-logo.png" alt="">
+      <AppHeader></AppHeader>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+img {
+  width: 4rem;
+  height: 4rem;
+}
+
+body {
+  font-family: 'Comic Neue', cursive;
+  font-family: 'Open Sans', sans-serif;
+}
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,7 +9,45 @@ export default {
 </script>
 
 <template>
-  <h1>ciao</h1>
+  <nav class="navbar navbar-expand-lg text-uppercase bg-color  fw-bold text-black">
+    <div class="container-fluid ">
+      <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Characters</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Comics</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Movies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Tv</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria="true">Games</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria="true">Collectibles</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria="true">Videos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria="true">Fans</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria="true">News</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style></style>
